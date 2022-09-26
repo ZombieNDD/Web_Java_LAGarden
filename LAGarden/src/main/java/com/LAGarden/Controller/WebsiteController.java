549d011a12e30957493b703587bbad8bde1da486
ZombieNDD <nguyen.d.dung00@gmail.com>
2022-09-27 01:16:47 +0700

    return

--- LAGarden/src/main/java/com/LAGarden/Controller/WebsiteController.java
+++ LAGarden/src/main/java/com/LAGarden/Controller/WebsiteController.java
@@ -5,15 +5,11 @@ import java.sql.SQLException;
 import org.apache.tiles.request.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.LAGarden.DAO.CTMonAnDAO;
-import com.LAGarden.DAO.DanhMucDAO;
 import com.LAGarden.DAO.ImageDatBanDAO;
-import com.LAGarden.Model.CTMonAn;
 import com.LAGarden.Model.DANGKY;
 @Controller
 public class WebsiteController {
@@ -44,39 +40,16 @@ public class WebsiteController {
         return "datban";
     }
 	@RequestMapping("/datmon")
-	public String datmon(ModelMap model) throws ClassNotFoundException, SQLException {
-		DanhMucDAO listDanhMuc = new DanhMucDAO();
-		CTMonAnDAO listDSMonAn = new CTMonAnDAO();
-		model.addAttribute("listDanhMuc",listDanhMuc.getListDanhMuc());
-		model.addAttribute("listMonAn",listDSMonAn.getListCTMonAN());
+	public String datmon(ModelMap model) {
 		return "datmon";
 	}
-	@RequestMapping(value = "/sanpham-{tensp}-{idsp}",method = RequestMethod.GET)
-	public String viewProduct(ModelMap model, @PathVariable("tensp") String tensp, @PathVariable("idsp") String idsp) throws ClassNotFoundException, SQLException{
-		DanhMucDAO listDanhMuc = new DanhMucDAO();
-		model.addAttribute("listDanhMuc",listDanhMuc.getListDanhMuc());
-		CTMonAnDAO ct = new CTMonAnDAO();
-		CTMonAn monAn = ct.chiTietMonAn(idsp);
-		model.addAttribute("monAn",monAn);
-		return "chitietsp";
-	}
-	@RequestMapping(value = "/danhmuc-{tenDanhMuc}-{danhMucID}",method = RequestMethod.GET)
-	public String listDanhMuc(ModelMap model, @PathVariable("tenDanhMuc") String tenDanhMuc, @PathVariable("danhMucID") String danhMucID) throws ClassNotFoundException, SQLException{
-		DanhMucDAO list = new DanhMucDAO();
-		CTMonAnDAO listMonAn = new  CTMonAnDAO();
-		model.addAttribute("listDanhMuc",list.getListDanhMuc());
-		model.addAttribute("listMonAn",listMonAn.getListByDanhMuc(danhMucID));
-		return "datmon";
-	}
-	
 	
 	@RequestMapping("/dangky")
-    public String dangky(ModelMap model) 
-    {
-
-        return "dangky";
-    }
-	
+	public String dangky( ModelMap model) {
+		//String tenTaiKhoan = model.;
+		
+		return "dangky";
+	}
 	
 	
 }
